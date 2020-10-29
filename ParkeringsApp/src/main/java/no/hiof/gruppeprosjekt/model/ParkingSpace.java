6cdcb02d4fb5c8a823504b3d825ed30e640468f8
Ramazan <ramazan_sanlioglu@live.com>
2020-10-29 15:24:53 +0100

    Oppdatert ParkingSpace.java
    La også til .class filen for ParkingSpace

--- ParkeringsApp/src/main/java/no/hiof/gruppeprosjekt/model/ParkingSpace.java
+++ ParkeringsApp/src/main/java/no/hiof/gruppeprosjekt/model/ParkingSpace.java
@@ -1,5 +1,70 @@
 package no.hiof.gruppeprosjekt.model;
 
-public class ParkingSpace {
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import java.sql.Time;
+import java.util.Random;
+
+public class ParkingSpace {
+    private String city;
+    private String address;
+    private double size_sqm;
+    private double price_ph;
+    private int spaceId;
+    //Skal se på muligheter for tid
+
+    Random rand = new Random();
+
+    public ParkingSpace(String city, String address, double size_sqm, double price_ph) {
+        this.city = city;
+        this.address = address;
+        this.size_sqm = size_sqm;
+        this.price_ph = price_ph;
+        spaceId = rand.nextInt(1000);
+    }
+
+    //Tom konstruktør for lesing av .json filer
+    public ParkingSpace() {
+
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSize_sqm() {
+        return size_sqm;
+    }
+
+    public void setSize_sqm(double size_sqm) {
+        this.size_sqm = size_sqm;
+    }
+
+    public double getPrice_ph() {
+        return price_ph;
+    }
+
+    public void setPrice_ph(double price_ph) {
+        this.price_ph = price_ph;
+    }
+
+    public int getSpaceId() {
+        return spaceId;
+    }
+
+    public void setSpaceId(int spaceId) {
+        this.spaceId = spaceId;
+    }
 }
