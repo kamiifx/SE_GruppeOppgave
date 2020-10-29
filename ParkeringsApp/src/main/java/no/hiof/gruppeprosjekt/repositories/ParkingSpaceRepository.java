b417f24421a123dd62f37aabf02a469f78378e9a
Ramazan <ramazan_sanlioglu@live.com>
2020-10-29 15:31:57 +0100

    Laget Repository for ParkingSpace

---
+++ ParkeringsApp/src/main/java/no/hiof/gruppeprosjekt/repositories/ParkingSpaceRepository.java
@@ -0,0 +1,8 @@
+package no.hiof.gruppeprosjekt.repositories;
+
+import java.util.ArrayList;
+
+//Akkurat nå leses det og skrives det i JSON siden vi ikke har noe database ute
+public class ParkingSpaceRepository {
+
+}
