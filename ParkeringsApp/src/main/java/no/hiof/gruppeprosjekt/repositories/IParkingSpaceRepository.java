efadc63fc8bc5b88eefac6a9006f711dc6a42a87
Ramazan <ramazan_sanlioglu@live.com>
2020-11-05 14:19:22 +0100

    Oppdatert Parkingspace, parkingspacerepo og controller

--- ParkeringsApp/src/main/java/no/hiof/gruppeprosjekt/repositories/IParkingSpaceRepository.java
+++ ParkeringsApp/src/main/java/no/hiof/gruppeprosjekt/repositories/IParkingSpaceRepository.java
@@ -1,5 +1,10 @@
 package no.hiof.gruppeprosjekt.repositories;
 
+import no.hiof.gruppeprosjekt.model.ParkingSpace;
+import java.util.ArrayList;
+
 public interface IParkingSpaceRepository {
+    ArrayList<ParkingSpace> getAllSpaces();
+    ParkingSpace getSpaceById(int id);
     void createParkingSpace(String city, String address, String size_sqm, String price_ph, String userId);
 }
