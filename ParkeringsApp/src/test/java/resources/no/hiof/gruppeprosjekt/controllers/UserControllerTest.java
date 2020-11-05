9bd2063b67f9a61d7c222a3c6b9daafe4f0f7ce3
Khayam <khayam.nami@hotmail.com>
2020-11-05 17:55:58 +0100

    Empty test bokser

---
+++ ParkeringsApp/src/test/java/resources/no/hiof/gruppeprosjekt/controllers/UserControllerTest.java
@@ -0,0 +1,59 @@
+package no.hiof.gruppeprosjekt.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+
+    @Test
+    void Create_User() {
+        
+    }
+
+    @Test
+    void Login_User() {
+
+    }
+
+    @Test
+    void Update_User() {
+
+    }
+
+    @Test
+    void Delete_User(){
+
+    }
+
+    @Test
+    void Create_Parking_Place(){
+
+    }
+
+    @Test
+    void Delete_Parking_Place(){
+
+    }
+
+    @Test
+    void Update_Parking_Place(){
+
+    }
+
+
+    /*
+
+    CreateUser
+    LoginUser
+    UpdateUser
+    DeleteUser
+
+    CreateParkingSpace
+    DeleteParkingSpace
+    UpdateParkingSpace
+
+
+     */
+}
