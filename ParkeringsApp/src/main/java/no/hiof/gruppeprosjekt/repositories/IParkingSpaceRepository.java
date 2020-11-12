304f0ffa2b087c93d48433453b2d5a4a165c4f90
Khayam <khayam.nami@hotmail.com>
2020-11-12 10:39:31 +0100

    Updated IParkingSpaceRepository

--- ParkeringsApp/src/main/java/no/hiof/gruppeprosjekt/repositories/IParkingSpaceRepository.java
+++ ParkeringsApp/src/main/java/no/hiof/gruppeprosjekt/repositories/IParkingSpaceRepository.java
@@ -4,13 +4,12 @@ import no.hiof.gruppeprosjekt.model.ParkingSpace;
 import java.util.ArrayList;
 
 public interface IParkingSpaceRepository {
-    ArrayList<ParkingSpace> getAllUser();
-    void deleteParkingspace (int spaceId);
-    ParkingSpace confirmParking (int spaceId);
+    //ArrayList<ParkingSpace> getAllUser();
+    //void deleteParkingspace (int spaceId);
+    //ParkingSpace confirmParking (int spaceId);
     ArrayList<ParkingSpace> getAllSpaces();
     ParkingSpace getSpaceById(int id);
     void createParkingSpace(String city, String address, String size_sqm, String price_ph, String userId);
-    void deleteParkingSpace(int spaceId);
-
-    void deleteUser(String user);
+    public void updateAvailability(int id, byte notAvailable);
+    //void deleteParkingSpace(int spaceId);
 }
